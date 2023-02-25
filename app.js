cad778069c5845254b305d8befd3fa41d2f6aa5e
thohan25 <thohan25@bergen.org>
2023-02-25 12:59:39 -0500

    progress

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-//set up the server
 const express = require( "express" );
 const logger = require("morgan");
-const db = require("./db_connection");
+const db = require("./db/db_connection");
 const app = express();
 const port = 8080;
 
-// define middleware that logs all incoming requests
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
+
 app.use(logger("dev"));
 
-// define middleware that serves static resources in the public directory
 app.use(express.static(__dirname + '/public'));
 
-// define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
-} );
+    res.render('index');
+});
 
-// define a route for the things inventory page
+const read_things_all_sql = `
+    SELECT 
+        id, item, quantity
+    FROM
+        things
+`
+
 app.get( "/things", ( req, res ) => {
-    db.execute('SELECT id, item, quantity FROM things', (error, results) => {
+    db.execute(read_things_all_sql, (error, results) => {
         if (error) {
             res.status(500).send(error);
         } else {
@@ -27,18 +32,29 @@ app.get( "/things", ( req, res ) => {
     })
 } );
 
-// define a route for the item detail page
-app.get( "/things/item", ( req, res ) => {
-    db.execute('SELECT id, item, quantity FROM things WHERE id = 0', (error, results) => {
-        if (error) {
+const read_item_sql = `
+    SELECT 
+        item, quantity, description 
+    FROM
+        things
+    WHERE
+        id = ?
+`
+
+app.get( "/things/item/:id", ( req, res ) => {
+    db.execute(read_item_sql, [req.params.id], (error, results) => {
+        if (error) 
             res.status(500).send(error);
-        } else {
-            res.send(results[0]);
+        else if (results.length == 0)
+            res.status(404).send(`No item found with id = "${req.params.id}"` );
+        else {
+            let data = results[0];
+            res.render('item', data);
         }
-    })
+    });
 } );
 
 // start the server
 app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
-} );
+} );    
